21e362c554192ba7a8dd7fa63060013e5f1ce275
chen <13439594353@163.com>
2022-12-22 20:14:19 +0800

    修改基础路径

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,7 @@
 /*
  * @Description: 配置 VitePress 站点的基本文件
  * @Date: 2022-12-22 20:00:00
- * @LastEditTime: 2022-12-22 20:00:31
+ * @LastEditTime: 2022-12-22 20:14:03
  */
 import { defineConfig } from 'vitepress'
 
@@ -9,5 +9,5 @@ export default defineConfig({
     title: '官方文档', // 站点的标题，浏览器 title 部分
     description: '一款开箱即用的 Vue 中后台管理系统框架，采用 Vue3 + Vite2 技术栈。',
     lang: 'zh-cn', // 语言
-    base: '/',
+    base: '/learn-docs/',
 })
