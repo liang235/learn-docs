96e67ee8909de9b1815a504eb8f5d8c9a3518756
chen <13439594353@163.com>
2023-08-14 18:20:30 +0800

    更新

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,22 +1,22 @@
 /*
  * @Description: 配置 VitePress 站点的基本文件
  * @Date: 2022-12-22 20:00:00
- * @LastEditTime: 2023-08-04 17:46:41
+ * @LastEditTime: 2023-08-14 18:18:53
  */
 import { defineConfig } from 'vitepress'
-import sidebar from './sidebar.js' // 侧边栏
-import nav from './nav.js' // 头部导航
+import sidebar from './sidebar.mjs' // 侧边栏
+import nav from './navbar.mjs' // 头部导航
 
 export default defineConfig({
 	title: '学习文档', // 站点的标题，浏览器 title 部分
 	description: '一款开箱即用的 Vue 中后台管理系统框架，采用 Vue3 + Vite2 技术栈',
 	lang: 'zh-cn', // 语言
 	base: '/learn-docs/',
-	head: [['link', { rel: 'icon', href: './vite.svg' }]],
+	head: [  ['link', { rel: 'icon', href: '/vitepress-logo-mini.svg' }]],
 
 	themeConfig: {
 		siteTitle: '学习文档', // 网站标题，默认情况下，nav 显示站点引用config.title值的标题
-		logo: '/vite.svg',
+		logo: { src: '/vitepress-logo-mini.svg', width: 24, height: 24 },
 
 		// 可用于自定义出现在上一个和下一个链接上方的文本
 		docFooter: {
@@ -44,19 +44,19 @@ export default defineConfig({
 		outline: 'deep',
 
 		// 搜索查询
-		// search: {
-		// 	provider: 'local',
-		// 	options: {
-		// 		translations: {
-		// 			button: { buttonText: '搜索文档', buttonAriaLabel: '搜索文档' },
-		// 			modal: {
-		// 				noResultsText: '无法找到相关结果',
-		// 				resetButtonTitle: '清除查询条件',
-		// 				footer: { selectText: '选择', navigateText: '切换', closeText: '关闭' },
-		// 			},
-		// 		},
-		// 	},
-		// },
+		search: {
+			provider: 'local',
+			options: {
+				translations: {
+					button: { buttonText: '搜索文档', buttonAriaLabel: '搜索文档' },
+					modal: {
+						noResultsText: '无法找到相关结果',
+						resetButtonTitle: '清除查询条件',
+						footer: { selectText: '选择', navigateText: '切换', closeText: '关闭' },
+					},
+				},
+			},
+		},
 	},
 
 	markdown: {
