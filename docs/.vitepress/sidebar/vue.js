64bb67175d412e8764a050c427aafd2b00789ea4
chen <13439594353@163.com>
2023-08-15 18:40:31 +0800

    更新

--- docs/.vitepress/sidebar/vue.js
+++ docs/.vitepress/sidebar/vue.js
@@ -12,13 +12,12 @@ export default [
 		collapsible: false, // 是否可折叠的侧边栏组
 		collapsed: false, // 是否展开侧边栏组，默认展开，设置为 true 为隐藏
 		items: [
-			{ text: '全局方法', link: `${commonPath}/global.md` },
+			{ text: '父子组件传参', link: `${commonPath}/father-son.md` },
 			{ text: 'watch 监听', link: `${commonPath}/watch.md` },
-			{ text: 'router 路由', link: `${commonPath}/router.md` },
 			{ text: ':deep() 样式穿透', link: `${commonPath}/:deep().md` },
 			{ text: 'Class 与 Style 绑定', link: `${commonPath}/class-style-bind.md` },
+			{ text: '全局方法', link: `${commonPath}/global.md` },
 			{ text: '异步组件', link: `${commonPath}/async-components.md` },
-			{ text: '$attrs', link: `${commonPath}/$attrs.md` },
 		],
 	},
 ]
--- docs/vue/$attrs.md
+++
@@ -1,2 +0,0 @@
-# $attrs
-一个包含了组件所有透传 attributes 的对象
